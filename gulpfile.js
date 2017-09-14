bb8eacf2f8edfd07c847cea901bb2cf7e415c3f1
Chathan Driehuys <cdriehuys@gmail.com>
2017-09-14 11:01:29 -0400

    Configure browser sync.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+
+  gulp.watch('**/*.{css,html}', reload);
+});
+
+
+gulp.task('default', ['serve']);
