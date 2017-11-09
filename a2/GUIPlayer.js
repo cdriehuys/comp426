6b02b9c2e7e81c255c11d8a8d793e8a4fe34f78b
Chathan Driehuys <chathan@driehuys.com>
2017-11-09 11:54:46 -0500

    Set up basic GUI player structure.

--- a2/GUIPlayer.js
+++ a2/GUIPlayer.js
@@ -1,6 +1,58 @@
 /**
  * A Hearts player that interacts with a GUI.
  */
-var GUIPlayer = function(name) {
-  this.name = name;
+var GUIPlayer = function(playerName) {
+  var currentGame;
+  var currentMatch;
+  var currentPosition;
+  var name = playerName;
+  var playerKey;
+
+  /**
+   * Get the player's name.
+   *
+   * @returns {string} The player's name.
+   */
+  this.getName = function() {
+    return name;
+  }
+
+  /**
+   * Set up a Hearts match.
+   *
+   * @param {Match} match The match being set up.
+   * @param {string} position The player's position.
+   */
+  this.setupMatch = function(match, position) {
+    currentMatch = match;
+    currentPosition = position;
+  }
+
+  /**
+   * Set up the next game of hearts.
+   *
+   * @param {GameOfHearts} game The game to set up.
+   * @param {string} key The key the player should use to authenticate their actions.
+   */
+  this.setupNextGame = function(game, key) {
+    currentGame = game;
+    playerKey = key;
+
+    game.registerEventHandler(Hearts.ALL_EVENTS, function(e) {
+      console.log('Received Event:', e.toString());
+    });
+  }
+
+  /**
+   * Handle an event of type GAME_STARTED_EVENT.
+   *
+   * @param {GameStartedEvent} event The even that occurred.
+   */
+  function handleGameStart(event) {
+    if (event.getPassType() === Hearts.PASS_NONE) {
+      console.log('No passing required.')
+
+      return;
+    }
+  }
 }
